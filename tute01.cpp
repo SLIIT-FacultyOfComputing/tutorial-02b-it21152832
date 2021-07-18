73b8650d466c96a577f92c317fd60f89765c9fc9
it21152832 <87379775+it21152832@users.noreply.github.com>
2021-07-18 05:31:22 +0000

    I fully completed tutorial 2

--- tute01.cpp
+++ tute01.cpp
@@ -17,14 +17,22 @@ printf("Hello World") --> std::cout << "Hello World";
 2.54cm = 1 inch
 */
 
+#include <iostream>
 
-#include <stdio.h>
-int main() 
+int main()
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
-    return 0;
-}  
+  //variable declaration
+  float cm, inches;
+
+//keyboard input
+std::cout<<"Enter a length in cm : ";
+std::cin>>cm;
+
+//calculation
+ inches = cm / 2.54;
+
+//output
+ std::cout<<"Length in inches is "<<inches<<std::endl;
+
+ return 0;
+}
--- tute03.cpp
+++ tute03.cpp
@@ -4,20 +4,28 @@ Convert the C program given below which calculates the Factorial of a number tha
 
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
+#include <iostream>
+
+int main() 
 {
-    int no;
-    long fac;
+  //variable declaration
+  int no;
+  long fac;
 
-    printf("Enter a Number : ");
-    scanf("%d", &no);
+  //keyboard input
+  std::cout<<"Enter a Number : ";
+  std::cin>>no;
 
-    fac = 1;
-    for (int r=no; r >= 1; r--) {
-        fac = fac * r;
-    }
+  //repitition
+  fac = 1;
+  for (int r=no; r >= 1; r--) 
+  {
+      fac = fac * r;
+  }
 
-    printf("Factorial of %d is %ld\n", no, fac);  
-    return 0;
+  //output
+  std::cout<<" Factorial of " << no << "is" << fac << std::endl; 
+
+  return 0;
+
 }
